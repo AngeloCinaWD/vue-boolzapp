85d1f6af756469442e8c5e40ec910c32768243cf
Angelo <angelo.cina@yahoo.it>
2020-11-23 17:13:47 +0100

    Selezione chat contatto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
-var app = new Vue ({ 
+var app = new Vue ({
+  el: "#app",
+  data: {
+    indiceContattoAttivo: 0,
+    contatti: [
+      {
+        nomeContatto: "Michele",
+        immagineContatto: "img/avatar_1.jpg",
+        messaggiChatInviati: ["Lo sai che ha aperto una nuova pizzeria?", "daje", "SSS"],
+        messaggiChatRicevuti: ["Si ma preferirei andare al cinema", "ok"]
+      },
+      {
+        nomeContatto: "Fabio",
+        immagineContatto: "img/avatar_2.jpg",
+        messaggiChatInviati: ["ggggggg", "daje", "rrr"],
+        messaggiChatRicevuti: ["Si"]
+      },
+      {
+        nomeContatto: "Samuele",
+        immagineContatto: "img/avatar_3.jpg",
+        messaggiChatInviati: ["Lo sai che ha aperto una nuova pizzeria?", "daje", "SSS"],
+        messaggiChatRicevuti: ["Si ma preferirei andare al cinema", "ok"]
+      },
+      {
+        nomeContatto: "Luisa",
+        immagineContatto: "img/avatar_4.jpg",
+        messaggiChatInviati: ["ggggggg", "daje", "rrr"],
+        messaggiChatRicevuti: ["Si"]
+      }
+    ]
+  },
+  methods: {
+    attivaContatto: function (indiceAttivo) {
+      this.indiceContattoAttivo = indiceAttivo;
+    }
+  }
 
 });
