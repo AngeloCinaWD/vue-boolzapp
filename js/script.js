18e964e4d397633d19b387d6f72c1164bd21d71a
Angelo <angelo.cina@yahoo.it>
2020-11-23 19:11:07 +0100

    Stampa messaggi chat secondo tipo e ordine

--- js/script.js
+++ js/script.js
@@ -6,26 +6,126 @@ var app = new Vue ({
       {
         nomeContatto: "Michele",
         immagineContatto: "img/avatar_1.jpg",
-        messaggiChatInviati: ["Lo sai che ha aperto una nuova pizzeria?", "daje", "SSS"],
-        messaggiChatRicevuti: ["Si ma preferirei andare al cinema", "ok"]
+        messaggiChat: [
+          {
+            testo: "Lo sai che ha aperto una nuova pizzeria?",
+            tipo: "sentMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Si ma preferirei andare al cinema",
+            tipo: "receivedMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Lo sai che ha aperto una nuova pizzeria?",
+            tipo: "sentMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Si ma preferirei andare al cinema",
+            tipo: "receivedMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Lo sai che ha aperto una nuova pizzeria?",
+            tipo: "sentMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          }
+        ]
       },
       {
         nomeContatto: "Fabio",
         immagineContatto: "img/avatar_2.jpg",
-        messaggiChatInviati: ["ggggggg", "daje", "rrr"],
-        messaggiChatRicevuti: ["Si"]
+        messaggiChat: [
+          {
+            testo: "sai che ha aperto una nuova pizzeria?",
+            tipo: "sentMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Si ma preferirei andare al cinema",
+            tipo: "receivedMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Lo sai che ha aperto una nuova pizzeria?",
+            tipo: "sentMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Si ma preferirei andare al cinema",
+            tipo: "receivedMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Lo sai che ha aperto una nuova pizzeria?",
+            tipo: "sentMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          }
+        ]
       },
       {
         nomeContatto: "Samuele",
         immagineContatto: "img/avatar_3.jpg",
-        messaggiChatInviati: ["Lo sai che ha aperto una nuova pizzeria?", "daje", "SSS"],
-        messaggiChatRicevuti: ["Si ma preferirei andare al cinema", "ok"]
+        messaggiChat: [
+          {
+            testo: "Lo sai che ha aperto una nuova pizzeria?",
+            tipo: "sentMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Si ma preferirei andare al cinema",
+            tipo: "receivedMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Lo sai che ha aperto una nuova pizzeria?",
+            tipo: "sentMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Si ma preferirei andare al cinema",
+            tipo: "receivedMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Lo sai che ha aperto una nuova pizzeria?",
+            tipo: "sentMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          }
+        ]
       },
       {
         nomeContatto: "Luisa",
         immagineContatto: "img/avatar_4.jpg",
-        messaggiChatInviati: ["ggggggg", "daje", "rrr"],
-        messaggiChatRicevuti: ["Si"]
+        messaggiChat: [
+          {
+            testo: "Lo sai che ha aperto una nuova pizzeria?",
+            tipo: "sentMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Si ma preferirei andare al cinema",
+            tipo: "receivedMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Lo sai che ha aperto una nuova pizzeria?",
+            tipo: "sentMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Si ma preferirei andare al cinema",
+            tipo: "receivedMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          },
+          {
+            testo: "Lo sai che ha aperto una nuova pizzeria?",
+            tipo: "sentMessage",
+            timeInfo: "20 11 2020 10:52:03"
+          }
+        ]
       }
     ]
   },
