a9e7f68f2a826e8381a9d006e62ac0b1889235d5
Angelo <angelo.cina@yahoo.it>
2020-11-25 16:19:19 +0100

    Funzione search fra i contatti

--- js/script.js
+++ js/script.js
@@ -2,11 +2,13 @@ var app = new Vue ({
   el: "#app",
   data: {
     inputMessaggio: "",
+    inputSearch: "",
     indiceContattoAttivo: 0,
     contatti: [
       {
         nomeContatto: "Michele",
         immagineContatto: "img/avatar_1.jpg",
+        visibile: true,
         messaggiChat: [
           {
             testo: "Lo sai che ha aperto una nuova pizzeria?",
@@ -28,6 +30,7 @@ var app = new Vue ({
       {
         nomeContatto: "Fabio",
         immagineContatto: "img/avatar_2.jpg",
+        visibile: true,
         messaggiChat: [
           {
             testo: "pizzeria?",
@@ -59,6 +62,7 @@ var app = new Vue ({
       {
         nomeContatto: "Samuele",
         immagineContatto: "img/avatar_3.jpg",
+        visibile: true,
         messaggiChat: [
           {
             testo: "Lo sai ?",
@@ -90,6 +94,7 @@ var app = new Vue ({
       {
         nomeContatto: "Luisa",
         immagineContatto: "img/avatar_4.jpg",
+        visibile: true,
         messaggiChat: [
           {
             testo: "fjsfjhhhh?",
@@ -134,16 +139,46 @@ var app = new Vue ({
     },
 
     inviaMessaggio: function () {
+      if (this.inputMessaggio != "")
       this.contatti[this.indiceContattoAttivo].messaggiChat.push({testo: this.inputMessaggio, tipo: "sentMessage", timeInfo: this.dataTime ()});
-      this.inputMessaggio = "";
+      // this.inputMessaggio = "";
     },
 
     messaggioRisposta: function () {
+      if (this.inputMessaggio != "")
       this.contatti[this.indiceContattoAttivo].messaggiChat.push({testo: "ok", tipo: "receivedMessage", timeInfo: this.dataTime ()});
+      this.inputMessaggio = "";
     },
 
     ritardoRisposta: function () {
       setTimeout (this.messaggioRisposta, 1000);
+    },
+
+    // cercaChat: function () {
+    //   console.log(this.inputSearch);
+    //   if (this.inputSearch != "")
+    //   var inputinserito = this.inputSearch;
+    //   this.contatti = this.contatti.filter((element) => {
+    //     // if (element.nomeContatto.includes(inputinserito)) {
+    //       return element.nomeContatto.includes(inputinserito)
+    //     // }
+    //   });
+    // }
+
+    cercaChat: function(){
+      this.contatti.forEach((elemento) => {
+        let inputInserito = app.inputSearch;
+        let nome = elemento.nomeContatto;
+
+        inputInserito = inputInserito.toLowerCase();
+        nome = nome.toLowerCase();
+
+        if (nome.includes(inputInserito)) {
+          elemento.visibile = true;
+        } else {
+          elemento.visibile = false;
+        }
+      });
     }
 
 
